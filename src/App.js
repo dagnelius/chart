a0c746c60d24c82f7acc2a4d5ba08b3e47340e74
Dags Neliuss <dag.nelius@gmail.com>
2023-03-21 20:45:30 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import LineChart from "./components/LineChart";
+import { Data } from "./Data";
+import { useState } from "react";
 function App() {
+  console.log(Data);
+  const [data, setData] = useState({
+    labels: [
+      "00:00",
+      "01:00",
+      "02:00",
+      "03:00",
+      "04:00",
+      "05:00",
+      "06:00",
+      "07:00",
+      "08:00",
+      "09:00",
+      "10:00",
+      "11:00",
+      "12:00",
+      "13:00",
+      "14:00",
+      "15:00",
+      "16:00",
+      "17:00",
+      "18:00",
+      "19:00",
+      "20:00",
+      "21:00",
+      "22:00",
+      "23:00",
+      "24:00",
+    ],
+    datasets: [
+      {
+        label: "Ceļa virsmas temperatūra [act] (C)",
+        data: [...Data.map((data) => data.temperature), -0.5],
+        borderColor: "red",
+        borderWidth: 2,
+        fill: false,
+      },
+      {
+        label: "Temperatūra -5cm [act] (C)",
+        data: [
+          ...Data.map(
+            (data) => data.temperature - (Math.random() * (0.5 - 0.1) + 0.1)
+          ),
+          -0.5,
+        ],
+        borderColor: "blue",
+        borderWidth: 2,
+        fill: false,
+      },
+      {
+        label: "Temperatūra -30cm [act] (C)",
+        data: [
+          ...Data.map((data) => -0.5 - (Math.random() * (0.1 - 0.05) + 0.05)),
+        ],
+        borderColor: "green",
+        borderWidth: 2,
+        fill: false,
+      },
+      {
+        label: "Sasalšanas temp. NaCl [act] (C)",
+        data: [
+          ...Data.map((data) => -0.65 - (Math.random() * (0.1 - 0.05) + 0.05)),
+        ],
+        borderColor: "rgb(255, 230, 0)",
+        borderWidth: 2,
+        fill: false,
+      },
+    ],
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ width: 1200 }}>
+        <LineChart chartData={data} />
+      </div>
     </div>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,127 @@
+export const Data = [
+  {
+    id: 1,
+    time: "0000-20230321-GMT2",
+    temperature: -2.5,
+  },
+  {
+    id: 2,
+    time: "0100-20230321-GMT2",
+    temperature: -2.4,
+  },
+  {
+    id: 3,
+    time: "0200-20230321-GMT2",
+    temperature: -2.3,
+  },
+  {
+    id: 4,
+    time: "0300-20230321-GMT2",
+    temperature: -2.3,
+  },
+  {
+    id: 5,
+    time: "0400-20230321-GMT2",
+    temperature: -2.3,
+  },
+  {
+    id: 6,
+    time: "0500-20230321-GMT2",
+    temperature: -2.1,
+  },
+  {
+    id: 7,
+    time: "0600-20230321-GMT2",
+    temperature: -1.9,
+  },
+  {
+    id: 1,
+    time: "0700-20230321-GMT2",
+    temperature: -1.9,
+  },
+  {
+    id: 8,
+    time: "0800-20230321-GMT2",
+    temperature: -1.8,
+  },
+  {
+    id: 9,
+    time: "0900-20230321-GMT2",
+    temperature: -1.8,
+  },
+  {
+    id: 10,
+    time: "1000-20230321-GMT2",
+    temperature: -1.7,
+  },
+  {
+    id: 11,
+    time: "1100-20230321-GMT2",
+    temperature: -1.5,
+  },
+  {
+    id: 12,
+    time: "1200-20230321-GMT2",
+    temperature: -1.2,
+  },
+  {
+    id: 13,
+    time: "1300-20230321-GMT2",
+    temperature: -1,
+  },
+  {
+    id: 14,
+    time: "1400-20230321-GMT2",
+    temperature: -0.8,
+  },
+  {
+    id: 15,
+    time: "1500-20230321-GMT2",
+    temperature: -0.8,
+  },
+  {
+    id: 16,
+    time: "1600-20230321-GMT2",
+    temperature: -0.9,
+  },
+  {
+    id: 17,
+    time: "1700-20230321-GMT2",
+    temperature: -1,
+  },
+  {
+    id: 18,
+    time: "1800-20230321-GMT2",
+    temperature: -1.1,
+  },
+  {
+    id: 19,
+    time: "1900-20230321-GMT2",
+    temperature: -1.3,
+  },
+  {
+    id: 20,
+    time: "2000-20230321-GMT2",
+    temperature: -1.5,
+  },
+  {
+    id: 21,
+    time: "2100-20230321-GMT2",
+    temperature: -1.8,
+  },
+  {
+    id: 22,
+    time: "2200-20230321-GMT2",
+    temperature: -2,
+  },
+  {
+    id: 23,
+    time: "2300-20230321-GMT2",
+    temperature: -2.3,
+  },
+  {
+    id: 24,
+    time: "2400-20230321-GMT2",
+    temperature: -2.4,
+  },
+];
